ba8db112d48f10de9a29e535b576597c7bd1422c
Daniel Beranek <Dan-Da@centrum.cz>
2023-08-28 09:36:38 +0200

    Solve tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temperatureFahrenheit) {
+  return +((temperatureFahrenheit - 32) * (5 / 9)).toFixed(1)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperatureCelsius) {
+  return +(temperatureCelsius * (9 / 5) + 32).toFixed(1)
 };
 
 // Do not edit below this line
