74af5813cfb018673d7170282d1a7f84d7a01ab0
Daniel Beranek <Dan-Da@centrum.cz>
2023-08-28 08:47:06 +0200

    Solve repeatString.js

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, numberOfRepeats) {
+  if (numberOfRepeats < 0) {
+    return "ERROR"
+  }
 
+
+  let repeatedString = "";
+
+  for (let i = 0; i < numberOfRepeats; i++) {
+    repeatedString += string
+  }
+
+  return repeatedString;
 };
 
 // Do not edit below this line
