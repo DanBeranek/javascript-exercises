7c7daa54641f593556252d971326aa343caaa8df
Daniel Beranek <Dan-Da@centrum.cz>
2023-08-28 09:04:37 +0200

    Solve removeFromArray.js

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...deleteValues) {
+  return array.filter(value => !deleteValues.includes(value));
 };
 
 // Do not edit below this line
