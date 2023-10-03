2c09b66d5dad8c0ac715211696533dd6410d527a
Daniel Beranek <Dan-Da@centrum.cz>
2023-10-03 21:29:05 +0200

    Solve fibonacci.js

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  if (n < 0) return 'OOPS';
+  let sequence = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    sequence.push(sequence[sequence.length-2] + sequence[sequence.length-1]);
+  }
+  return sequence[sequence.length-1]
 };
 
 // Do not edit below this line
